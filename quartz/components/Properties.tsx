a0a7b70cd2bf33257e3ce7d77ddd7e68b3e13a59
Michele Papucci <m.papucci@studenti.unipi.it>
2024-09-19 15:36:05 +0200

    Quartz sync: Sep 19, 2024, 3:36 PM

--- quartz/components/Properties.tsx
+++ quartz/components/Properties.tsx
@@ -1,79 +1,79 @@
-import { FullSlug, stripSlashes, TransformOptions, transformLink} from "../util/path"
+import { FullSlug, stripSlashes, TransformOptions, transformLink } from "../util/path"
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import  style from "./styles/properties.scss"
+import style from "./styles/properties.scss"
 
-function createLinkedElement(fileData: any, opts : any, value: string) {
-  let cleanedValue = value.replace(/['"\[\]]+/g, '')
-  let href = transformLink(fileData.slug!, cleanedValue, opts)
+function createLinkedElement(fileData: any, opts: any, value: string) {
+    let cleanedValue = value.replace(/['"\[\]]+/g, '')
+    let href = transformLink(fileData.slug!, cleanedValue, opts)
 
-  return (
-    <a  href={href} class="internal">{cleanedValue}</a>
-  )
+    return (
+        <a href={href} class="internal">{cleanedValue}</a>
+    )
 }
 
 function createPropertyElement(key: string, value: any) {
-  return(
-    <li>
-      <span class="property">{key}</span> : <span class="value">{value}</span>
-    </li>
-  )
+    return (
+        <li>
+            <span class="property">{key}</span> : <span class="value">{value}</span>
+        </li>
+    )
 }
 
 export default (() => {
-  function PropertiesWithWorkingLinks({fileData, allFiles}: QuartzComponentProps, ) {
-    const opts: TransformOptions = {
-      strategy: "shortest",
-      allSlugs: allFiles.map((fp) => fp.slug as FullSlug)
-    }
+    function PropertiesWithWorkingLinks({ fileData, allFiles }: QuartzComponentProps,) {
+        const opts: TransformOptions = {
+            strategy: "shortest",
+            allSlugs: allFiles.map((fp) => fp.slug as FullSlug)
+        }
 
-    var propertiesElements = []
+        var propertiesElements = []
 
-    if(Object.keys(fileData.frontmatter ?? {}).length > 0){
-      for (const [key, value] of Object.entries(fileData.frontmatter ?? {})) {
-        if(key.includes("draft")||key.includes("title")||key.includes("tags")||key.includes("aliases")){} // Add properties you want to ignore here
-        else{
-          var linkedElements = []
-          var propertyType = Object.prototype.toString.call(value)
-          if(value) {
-            if (propertyType = "[object String]" && value.includes("[[")){        //Check if it's a string or string array
-                if value.includes("|") {
-                    linkedElements.push(createLinkedElement(fileData, opts, value.split("|")[0]))
-                }
-                linkedElements.push(createLinkedElement(fileData, opts, value))
-              }
-              else if(propertyType = "[object Array]"){
-                for (const [index, arrayItem] of Object.entries(value ?? {})) {     // Check if it's an array
-                  var entry = value[index]
-                  if(entry.includes("[[")){
-                    if(Number(index) > 0){
-                      linkedElements.push(", ")
+        if (Object.keys(fileData.frontmatter ?? {}).length > 0) {
+            for (const [key, value] of Object.entries(fileData.frontmatter ?? {})) {
+                if (key.includes("draft") || key.includes("title") || key.includes("tags") || key.includes("aliases")) { } // Add properties you want to ignore here
+                else {
+                    var linkedElements = []
+                    var propertyType = Object.prototype.toString.call(value)
+                    if (value) {
+                        if (propertyType = "[object String]" && value.includes("[[")) {        //Check if it's a string or string array
+                            if (value.includes("|")) {
+                                linkedElements.push(createLinkedElement(fileData, opts, value.split("|")[0]))
+                            }
+                            linkedElements.push(createLinkedElement(fileData, opts, value))
+                        }
+                        else if (propertyType = "[object Array]") {
+                            for (const [index, arrayItem] of Object.entries(value ?? {})) {     // Check if it's an array
+                                var entry = value[index]
+                                if (entry.includes("[[")) {
+                                    if (Number(index) > 0) {
+                                        linkedElements.push(", ")
+                                    }
+                                    linkedElements.push(createLinkedElement(fileData, opts, entry))
+                                }
+                                else {
+                                    linkedElements.push(entry)
+                                }
+                            }
+                        }
                     }
-                    linkedElements.push(createLinkedElement(fileData, opts, entry))
-                  }
-                  else{
-                    linkedElements.push(entry)
-                  }
+                    propertiesElements.push(createPropertyElement(key, linkedElements))
                 }
-              }
-          }
-        propertiesElements.push(createPropertyElement(key, linkedElements))
-      }
 
-      }
+            }
+        }
+
+        return (
+            <div class="properties">
+                <ul>{propertiesElements}</ul>
+            </div>
+        )
+
     }
-
-    return (      
-      <div class="properties">
-        <ul>{propertiesElements}</ul>
-      </div>
-    )
-
-  }
 
     PropertiesWithWorkingLinks.css = style
     return PropertiesWithWorkingLinks
 
-  }
+}
 ) satisfies QuartzComponentConstructor
 
 /* Credit to both https://github.com/gamberoillecito for their author and nextnote from which I built this and to https://github.com/jackyzha0 both the creator and gracious question answerer*/
